b57f2a98dcee5f7f65284ae6ea0e98875657b40c
Ivan Kozik <ivan@ludios.org>
2014-07-05 23:38:41 +0000

    Make Rust conway actually work

--- borrowing.rs
+++ borrowing.rs
@@ -1,14 +1,17 @@
-fn mutate_me_2(grid: &mut [uint, ..10]) {
-	grid[0] += 1;
+// https://github.com/rust-lang/rust/issues/13859
+struct S { s: [int, ..10] }
+
+fn mutate_me_2(grid: &mut S) {
+	grid.s[0] += 1;
 }
 
-fn mutate_me(grid: &mut [uint, ..10]) {
+fn mutate_me(grid: &mut S) {
 	mutate_me_2(grid);
-	grid[0] += 1;
+	grid.s[0] += 1;
 }
 
 fn main() {
-	let mut grid = [0, ..10];
+	let mut grid = S { s: [0, ..10] };
 	mutate_me(&mut grid);
-	println!("{}", grid[0]);
+	println!("{}", grid.s[0]);
 }
--- conway.rs
+++ conway.rs
@@ -1,52 +1,50 @@
 extern crate rand;
 
-static ROWS: uint = 78;
-static COLS: uint = 20;
+static ROWS: uint = 177;
+static COLS: uint = 60;
 
-type Grid = [[u8, ..ROWS], ..COLS];
-type GhostGrid = [[u8, ..ROWS+2], ..COLS+2];
+type Grid = [[u8, ..ROWS+1], ..COLS+1];
+type GhostGrid = [[u8, ..ROWS+1+2], ..COLS+1+2];
 
-// TODO: can I keep `grid` not-mut?
 /**
  * A virtual grid that includes wrapped edges, so that we don't have to
  * do funky modulo arithmetic.
  */
-fn update_ghost(grid: &mut Grid, ghost_grid: &mut GhostGrid) -> () {
+fn update_ghost(grid: &Grid, ghost_grid: &mut GhostGrid) -> () {
 	/* Copy bottom of grid to top of ghost_grid */
 	for n in range(0u, ROWS) {
-		ghost_grid[n+1][0] = grid[n+1][COLS-1];
+		ghost_grid[0][n+1] = grid[COLS-1][n+1];
 	}
 
 	/* Copy top of grid to bottom of ghost_grid */
 	for n in range(0u, ROWS) {
-		ghost_grid[n+1][COLS+2-1] = grid[n+1][0];
+		ghost_grid[COLS+2-1][n+1] = grid[0][n+1];
 	}
 
 	/* Copy the rest of grid to ghost_grid */
 	for y in range(0u, COLS) {
 		for x in range(0u, ROWS) {
-			ghost_grid[x+1][y+1] = grid[x][y];
+			ghost_grid[y+1][x+1] = grid[y][x];
 		}
 	}
 
 	/* Wrap ghost_grid left and right columns */
 	for y in range(0u, COLS+2) {
-		ghost_grid[0][y] = ghost_grid[ROWS+2-2][y];
-		ghost_grid[ROWS+2-2][y] = ghost_grid[1][y];
+		ghost_grid[y][0] = ghost_grid[y][ROWS+2-2];
+		ghost_grid[y][ROWS+2-2] = ghost_grid[y][1];
 	}
 }
 
-// TODO: can I keep this not-mut?
-fn count_neighbors(x: uint, y: uint, ghost_grid: &mut GhostGrid) -> u8 {
-	ghost_grid[(x-1)+1][(y-1)+1] + ghost_grid[(x)+1][(y-1)+1] + ghost_grid[(x+1)+1][(y-1)+1] +
-	ghost_grid[(x-1)+1][(y)+1  ]                              + ghost_grid[(x+1)+1][(y)+1  ] +
-	ghost_grid[(x-1)+1][(y+1)+1] + ghost_grid[(x)+1][(y+1)+1] + ghost_grid[(x+1)+1][(y+1)+1]
+fn count_neighbors(x: uint, y: uint, ghost_grid: &GhostGrid) -> u8 {
+	ghost_grid[(y-1)+1][(x-1)+1] + ghost_grid[(y-1)+1][(x)+1] + ghost_grid[(y-1)+1][(x+1)+1] +
+	ghost_grid[(y)+1  ][(x-1)+1]                              + ghost_grid[(y)+1  ][(x+1)+1] +
+	ghost_grid[(y+1)+1][(x-1)+1] + ghost_grid[(y+1)+1][(x)+1] + ghost_grid[(y+1)+1][(x+1)+1]
 }
 
 fn pretty_print(grid: &Grid) -> () {
 	for y in range(0u, COLS) {
 		for x in range(0u, ROWS) {
-			if(grid[x][y] == 0) {
+			if grid[y][x] == 0 {
 				print!(" ");
 			} else {
 				print!("{}", "#");
@@ -59,33 +57,37 @@ fn pretty_print(grid: &Grid) -> () {
 fn next_gen(grid: &mut Grid, ghost_grid: &mut GhostGrid) -> () {
 	for y in range(0u, COLS) {
 		for x in range(0u, ROWS) {
-			let neighbors = count_neighbors(x, y, ghost_grid);
+			let neighbors = count_neighbors(x, y, &*ghost_grid);
 			//print!("N %d, ", neighbors);
-			if(neighbors < 2 || neighbors > 3) {
-				grid[x][y] = 0;
-			} else if(neighbors == 3) {
-				grid[x][y] = 1;
+			if neighbors < 2 || neighbors > 3 {
+				grid[y][x] = 0;
+			} else if neighbors == 3 {
+				grid[y][x] = 1;
 			}
 		}
 	}
-	update_ghost(grid, ghost_grid);
+	update_ghost(&*grid, ghost_grid);
 }
 
 fn main() {
-	let mut grid: Grid = [[0, ..ROWS], ..COLS];
-	let mut ghost_grid: GhostGrid = [[0, ..ROWS+2], ..COLS+2];
+	let mut grid: Grid = [[0, ..ROWS+1], ..COLS+1];
+	let mut ghost_grid: GhostGrid = [[0, ..ROWS+1+2], ..COLS+1+2];
 
 	/* Generate a random grid */
 	for y in range(0u, COLS) {
 		for x in range(0u, ROWS) {
-			grid[x][y] = (rand::random::<uint>() % 2) as u8;
+			grid[y][x] = (rand::random::<uint>() % 2) as u8;
 		}
 	}
 
-	update_ghost(&mut grid, &mut ghost_grid);
+	update_ghost(&grid, &mut ghost_grid);
 
 	for _ in range(0, 1000000) {
+		println!(" ");
+		println!(" ");
+		println!(" ");
 		pretty_print(&grid);
+		std::io::timer::sleep(140);
 		next_gen(&mut grid, &mut ghost_grid);
 	}
 }
