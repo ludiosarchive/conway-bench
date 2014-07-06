f8cad5095c70c2961c44dd4539ab42c799897af4
Ivan Kozik <ivan@ludios.org>
2014-07-06 03:03:55 +0000

    Fix ROWS/COLS

--- conway.c
+++ conway.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ROWS 177
-#define COLS 60
+#define COLS 177
+#define ROWS 60
 
 #define GRID_TYPE unsigned short int
 
-static void update_ghost(GRID_TYPE grid[ROWS][COLS], GRID_TYPE ghost_grid[ROWS+2][COLS+2]) {
+static void update_ghost(GRID_TYPE grid[COLS][ROWS], GRID_TYPE ghost_grid[COLS+2][ROWS+2]) {
 	/*
 	A virtual grid that includes wrapped edges, so that we don't have to
 	do funky modulo arithmetic.
@@ -15,30 +15,30 @@ static void update_ghost(GRID_TYPE grid[ROWS][COLS], GRID_TYPE ghost_grid[ROWS+2
 	int n, x, y;
 
 	/* Copy bottom of grid to top of ghost_grid */
-	for(n=0; n <= ROWS-1; n++) {
-		ghost_grid[n+1][0] = grid[n+1][COLS-1];
+	for(n=0; n <= COLS-1; n++) {
+		ghost_grid[n+1][0] = grid[n+1][ROWS-1];
 	}
 
 	/* Copy top of grid to bottom of ghost_grid */
-	for(n=0; n <= ROWS-1; n++) {
-		ghost_grid[n+1][COLS+2-1] = grid[n+1][0];
+	for(n=0; n <= COLS-1; n++) {
+		ghost_grid[n+1][ROWS+2-1] = grid[n+1][0];
 	}
 
 	/* Copy the rest of grid to ghost_grid */
-	for(y=0; y <= COLS-1; y++) {
-		for(x=0; x <= ROWS-1; x++) {
+	for(y=0; y <= ROWS-1; y++) {
+		for(x=0; x <= COLS-1; x++) {
 			ghost_grid[x+1][y+1] = grid[x][y];
 		}
 	}
 
 	/* Wrap ghost_grid left and right columns */
-	for(y=0; y <= COLS+2-1; y++) {
-		ghost_grid[0][y] = ghost_grid[ROWS+2-2][y];
-		ghost_grid[ROWS+2-2][y] = ghost_grid[1][y];
+	for(y=0; y <= ROWS+2-1; y++) {
+		ghost_grid[0][y] = ghost_grid[COLS+2-2][y];
+		ghost_grid[COLS+2-2][y] = ghost_grid[1][y];
 	}
 }
 
-static int count_neighbors(int x, int y, GRID_TYPE ghost_grid[ROWS+2][COLS+2]) {
+static int count_neighbors(int x, int y, GRID_TYPE ghost_grid[COLS+2][ROWS+2]) {
 	return \
 		ghost_grid[(x-1)+1][(y-1)+1] + ghost_grid[(x)+1][(y-1)+1] + ghost_grid[(x+1)+1][(y-1)+1] + \
 		ghost_grid[(x-1)+1][(y)+1  ]                              + ghost_grid[(x+1)+1][(y)+1  ] + \
@@ -51,14 +51,14 @@ static int get_random(N) {
 	return val;
 }
 
-static void pretty_print(GRID_TYPE grid[ROWS][COLS]) {
+static void pretty_print(GRID_TYPE grid[COLS][ROWS]) {
 	int x, y;
 
-	char out[(ROWS+1)*COLS];
+	char out[(COLS+1)*ROWS];
 	int out_i = 0;
 
-	for(y=0; y <= COLS-1; y++) {
-		for(x=0; x <= ROWS-1; x++) {
+	for(y=0; y <= ROWS-1; y++) {
+		for(x=0; x <= COLS-1; x++) {
 			if(grid[x][y] == 0) {
 				out[out_i] = ' ';
 				out_i += 1;
@@ -73,10 +73,10 @@ static void pretty_print(GRID_TYPE grid[ROWS][COLS]) {
 	puts(out);
 }
 
-static void next_gen(GRID_TYPE grid[ROWS][COLS], GRID_TYPE ghost_grid[ROWS+2][COLS+2]) {
+static void next_gen(GRID_TYPE grid[COLS][ROWS], GRID_TYPE ghost_grid[COLS+2][ROWS+2]) {
 	int neighbors, x, y;
-	for(y=0; y <= COLS-1; y++) {
-		for(x=0; x <= ROWS-1; x++) {
+	for(y=0; y <= ROWS-1; y++) {
+		for(x=0; x <= COLS-1; x++) {
 			neighbors = count_neighbors(x, y, ghost_grid);
 			//printf("N %d, ", neighbors);
 			if(neighbors < 2 || neighbors > 3) {
@@ -93,8 +93,8 @@ static void next_gen(GRID_TYPE grid[ROWS][COLS], GRID_TYPE ghost_grid[ROWS+2][CO
 static struct timespec wait = {0, 160 * 1000 * 1000};
 
 int main() {
-	GRID_TYPE grid[ROWS][COLS];
-	GRID_TYPE ghost_grid[ROWS+2][COLS+2];
+	GRID_TYPE grid[COLS][ROWS];
+	GRID_TYPE ghost_grid[COLS+2][ROWS+2];
 	int character;
 	int x;
 	int y;
@@ -102,8 +102,8 @@ int main() {
 	srand(time(0));
 
 	/* Generate a random grid */
-	for(y=0; y <= COLS-1; y++) {
-		for(x=0; x <= ROWS-1; x++) {
+	for(y=0; y <= ROWS-1; y++) {
+		for(x=0; x <= COLS-1; x++) {
 			grid[x][y] = get_random(2);
 		}
 	}
