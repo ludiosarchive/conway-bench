3e8cde42d952ae2b04d66efb1bbdb49b938ec361
Ivan Kozik <ivan@ludios.org>
2014-07-25 23:29:45 +0000

    Add JavaScript TypeArray impl, 3.8x slower than the Array impl

---
+++ conway_typedarray.js
@@ -0,0 +1,121 @@
+var COLS = 177;
+var ROWS = 60;
+
+var update_ghost = function(grid, ghost_grid) {
+	/*
+	A virtual grid that includes wrapped edges, so that we don't have to
+	do funky modulo arithmetic.
+	*/
+
+	var n, x, y;
+
+	/* Copy bottom of grid to top of ghost_grid */
+	for(n=0; n < COLS; n++) {
+		ghost_grid.set(0, n+1, grid.get(ROWS-1, n));
+	}
+
+	/* Copy top of grid to bottom of ghost_grid */
+	for(n=0; n < COLS; n++) {
+		ghost_grid.set(ROWS+2-1, n+1, grid.get(0, n));
+	}
+
+	/* Copy the rest of grid to ghost_grid */
+	for(y=0; y < ROWS; y++) {
+		for(x=0; x < COLS; x++) {
+			ghost_grid.set(y+1, x+1, grid.get(y, x));
+		}
+	}
+
+	/* Wrap ghost_grid left and right columns */
+	for(y=0; y < ROWS+2; y++) {
+		ghost_grid.set(y, 0, ghost_grid.get(y, COLS+2-2));
+		ghost_grid.set(y, COLS+2-1, ghost_grid.get(y, 1));
+	}
+}
+
+var count_neighbors = function(x, y, ghost_grid) {
+	return (
+		ghost_grid.get((y-1)+1, (x-1)+1) + ghost_grid.get((y-1)+1, (x)+1) + ghost_grid.get((y-1)+1, (x+1)+1) +
+		ghost_grid.get((y  )+1, (x-1)+1)                                  + ghost_grid.get((y  )+1, (x+1)+1) +
+		ghost_grid.get((y+1)+1, (x-1)+1) + ghost_grid.get((y+1)+1, (x)+1) + ghost_grid.get((y+1)+1, (x+1)+1));
+}
+
+var pretty_print = function(grid) {
+	var x, y;
+
+	var out = "";
+
+	for(y=0; y <= ROWS-1; y++) {
+		for(x=0; x <= COLS-1; x++) {
+			if(grid.get(y, x) == 0) {
+				out += ' ';
+			} else {
+				out += '#';
+			}
+		}
+		out += '\n';
+	}
+	process.stdout.write(out);
+}
+
+var next_gen = function(grid, ghost_grid) {
+	var neighbors, x, y;
+	for(y=0; y < ROWS; y++) {
+		for(x=0; x < COLS; x++) {
+			neighbors = count_neighbors(x, y, ghost_grid);
+			//printf("N %d, ", neighbors);
+			if(neighbors < 2 || neighbors > 3) {
+				grid.set(y, x, 0);
+			} else if(neighbors == 3) {
+				grid.set(y, x, 1);
+			}
+		}
+	}
+	update_ghost(grid, ghost_grid);
+}
+
+var make_grid = function(width, height) {
+	var arr = Uint8Array(width * height);
+	return {
+		get: function(y, x) { return arr[y*COLS + x]; },
+		set: function(y, x, val) { arr[y*COLS + x] = val; },
+	}
+}
+
+var run_once = function() {
+	var grid = make_grid(COLS, ROWS);
+	var ghost_grid = make_grid(COLS + 2, ROWS + 2);
+	var x;
+	var y;
+
+	/* Generate a random grid */
+	for(y=0; y < ROWS; y++) {
+		for(x=0; x < COLS; x++) {
+			grid.set(y, x, Math.floor(Math.random() * 2));
+		}
+	}
+
+	pretty_print(grid);
+	update_ghost(grid, ghost_grid);
+
+	var iterations = 5000;
+
+	while(iterations--) {
+		process.stdout.write("\n\n\n");
+		pretty_print(grid);
+		//nanosleep(&wait, NULL);
+
+		next_gen(grid, ghost_grid);
+	}
+}
+
+var main = function() {
+	run_once();
+	process.stderr.write("Warmed up.\n");
+	var start = Date.now();
+	run_once()
+	var end = Date.now();
+	process.stderr.write("Ran in " + (end - start) + "ms\n");
+}
+
+main();
