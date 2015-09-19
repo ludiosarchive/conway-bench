656575d0fccea74fad224983c32a48a91d5330f3
Ivan Kozik <ivan@ludios.org>
2015-09-19 17:51:57 +0000

    Start porting to Rust 1.3+

--- build_rust.sh
+++ build_rust.sh
@@ -1,1 +1,1 @@
-rustc --opt-level=3 -Z no-landing-pads -Z lto conway.rs
+rustc -C opt-level=3 -Z no-landing-pads -C target-cpu=native -C no-stack-check -C lto conway.rs
--- src/conway.rs
+++ src/conway.rs
@@ -1,13 +1,17 @@
 extern crate rand;
-use std::io::stdio;
-use std::rand;
+use std::io::{self, Write};
 use std::str;
+use rand::Rng;
 
-static COLS: uint = 177;
-static ROWS: uint = 60;
+const COLS: usize = 177;
+const ROWS: usize = 60;
 
-type Grid = [[u8, ..COLS], ..ROWS];
-type GhostGrid = [[u8, ..COLS+2], ..ROWS+2];
+type Grid = [[u8; COLS]; ROWS];
+type GhostGrid = [[u8; COLS+2]; ROWS+2];
+
+fn print(s: &[u8]) {
+	std::io::stdout().write(s);
+}
 
 /**
  * A virtual grid that includes wrapped edges, so that we don't have to
@@ -15,30 +19,30 @@ type GhostGrid = [[u8, ..COLS+2], ..ROWS+2];
  */
 fn update_ghost(grid: &Grid, ghost_grid: &mut GhostGrid) -> () {
 	/* Copy bottom of grid to top of ghost_grid */
-	for n in range(0u, COLS) {
+	for n in 0..COLS {
 		ghost_grid[0][n+1] = grid[ROWS-1][n];
 	}
 
 	/* Copy top of grid to bottom of ghost_grid */
-	for n in range(0u, COLS) {
+	for n in 0..COLS {
 		ghost_grid[ROWS+2-1][n+1] = grid[0][n];
 	}
 
 	/* Copy the rest of grid to ghost_grid */
-	for y in range(0u, ROWS) {
-		for x in range(0u, COLS) {
+	for y in 0..ROWS {
+		for x in 0..COLS {
 			ghost_grid[y+1][x+1] = grid[y][x];
 		}
 	}
 
 	/* Wrap ghost_grid left and right columns */
-	for y in range(0u, ROWS+2) {
+	for y in 0..ROWS+2 {
 		ghost_grid[y][0] = ghost_grid[y][COLS+2-2];
 		ghost_grid[y][COLS+2-1] = ghost_grid[y][1];
 	}
 }
 
-fn count_neighbors(x: uint, y: uint, ghost_grid: &GhostGrid) -> u8 {
+fn count_neighbors(x: usize, y: usize, ghost_grid: &GhostGrid) -> u8 {
 	ghost_grid[(y-1)+1][(x-1)+1] + ghost_grid[(y-1)+1][(x)+1] + ghost_grid[(y-1)+1][(x+1)+1] +
 	ghost_grid[(y)+1  ][(x-1)+1]                              + ghost_grid[(y)+1  ][(x+1)+1] +
 	ghost_grid[(y+1)+1][(x-1)+1] + ghost_grid[(y+1)+1][(x)+1] + ghost_grid[(y+1)+1][(x+1)+1]
@@ -46,10 +50,10 @@ fn count_neighbors(x: uint, y: uint, ghost_grid: &GhostGrid) -> u8 {
 
 fn pretty_print(grid: &Grid) -> () {
 	// COLS+1 for the newline
-	let mut out: [u8, ..(ROWS*(COLS+1))] = [0, ..(ROWS*(COLS+1))];
-	let mut out_i: uint = 0;
-	for y in range(0u, ROWS) {
-		for x in range(0u, COLS) {
+	let mut out: [u8; (ROWS*(COLS+1))] = [0; (ROWS*(COLS+1))];
+	let mut out_i: usize = 0;
+	for y in 0..ROWS {
+		for x in 0..COLS {
 			if grid[y][x] == 0 {
 				out[out_i] = ' ' as u8;
 				out_i += 1;
@@ -63,13 +67,13 @@ fn pretty_print(grid: &Grid) -> () {
 	}
 	unsafe {
 		let s = str::raw::from_utf8(out);
-		stdio::print(s);
+		print(s);
 	}
 }
 
 fn next_gen(grid: &mut Grid, ghost_grid: &mut GhostGrid) -> () {
-	for y in range(0u, ROWS) {
-		for x in range(0u, COLS) {
+	for y in 0..ROWS {
+		for x in 0..COLS {
 			let neighbors = count_neighbors(x, y, &*ghost_grid);
 			//print!("N %d, ", neighbors);
 			if neighbors < 2 || neighbors > 3 {
@@ -83,20 +87,20 @@ fn next_gen(grid: &mut Grid, ghost_grid: &mut GhostGrid) -> () {
 }
 
 fn main() {
-	let mut grid: Grid = [[0, ..COLS], ..ROWS];
-	let mut ghost_grid: GhostGrid = [[0, ..COLS+2], ..ROWS+2];
+	let mut grid: Grid = [[0; COLS]; ROWS];
+	let mut ghost_grid: GhostGrid = [[0; COLS+2]; ROWS+2];
 
 	/* Generate a random grid */
-	for y in range(0u, ROWS) {
-		for x in range(0u, COLS) {
-			grid[y][x] = (rand::random::<uint>() % 2) as u8;
+	for y in 0..ROWS {
+		for x in 0..COLS {
+			grid[y][x] = (rand::random::<usize>() % 2) as u8;
 		}
 	}
 
 	update_ghost(&grid, &mut ghost_grid);
 
-	for _ in range(0u, 10000) {
-		stdio::print("\n\n\n");
+	for _ in 0..10000 {
+		print!("\n\n\n");
 		pretty_print(&grid);
 		//std::io::timer::sleep(160);
 		next_gen(&mut grid, &mut ghost_grid);
